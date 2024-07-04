92bb80d154e2f88bf06800e1ee5bb2e4c10b26c8
joaoLouceiro <jrlouceiro@live.com>
2024-07-04 15:07:44 +0100

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,46 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
-    String input;
+    boolean isRunning = true;
     do {
       System.out.print("$ ");
-      input = scanner.nextLine();
-      if (!input.equals("exit 0"))
-        System.out.printf("%s: command not found\n", input);
-    } while (!input.equals("exit 0"));
+      String input = scanner.nextLine();
+      String[] shellArgs = input.split(" ");
+      isRunning = callCommand(shellArgs);
+    } while (isRunning);
     scanner.close();
+  }
+
+  private static boolean callCommand(String[] args) {
+    switch (args[0]) {
+      case "exit":
+        return exit(args);
+      case "echo":
+        return echo(args);
+      default:
+        return notFound(args[0]);
+    }
+  }
+
+  private static boolean echo(String[] args) {
+    System.out.println(String.join(" ", Arrays.copyOfRange(args, 1, args.length)));
+    return true;
+  }
+
+  private static boolean exit(String[] args) {
+    if (args.length == 1 || !args[1].equals("0")) {
+      System.out.println("Invalid argument");
+      return true;
+    }
+    return false;
+  }
+
+  private static boolean notFound(String input) {
+    System.out.printf("%s: command not found\n", input);
+    return true;
   }
 
 }
