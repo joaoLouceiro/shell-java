a50ef305cd2b7bd1999d40a76a3be4d733b3f9c1
joaoLouceiro <jrlouceiro@live.com>
2024-07-04 15:59:58 +0100

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+  static List<String> declaredMethods = new ArrayList<>();
+
   public static void main(String[] args) throws Exception {
+
+    getDeclaredMethods();
+
     Scanner scanner = new Scanner(System.in);
     boolean isRunning = true;
     do {
@@ -20,9 +28,24 @@ public class Main {
         return exit(args);
       case "echo":
         return echo(args);
+      case "type":
+        return type(args);
       default:
         return notFound(args[0]);
     }
+  }
+
+  private static boolean type(String[] args) {
+    if (args.length != 2) {
+      return invalidArgument();
+    }
+
+    if (declaredMethods.contains(args[1])) {
+      System.out.printf("%s is a shell builtin\n", args[1]);
+    } else {
+      System.out.printf("%s: not found\n", args[1]);
+    }
+    return true;
   }
 
   private static boolean echo(String[] args) {
@@ -31,9 +54,8 @@ public class Main {
   }
 
   private static boolean exit(String[] args) {
-    if (args.length == 1 || !args[1].equals("0")) {
-      System.out.println("Invalid argument");
-      return true;
+    if (args.length != 2 || !args[1].equals("0")) {
+      return invalidArgument();
     }
     return false;
   }
@@ -43,4 +65,19 @@ public class Main {
     return true;
   }
 
+  private static boolean invalidArgument() {
+    System.out.println("Invalid argument");
+    return true;
+  }
+
+  private static void getDeclaredMethods() throws ClassNotFoundException {
+    Class<?> c = Class.forName("Main");
+    Method m[] = c.getDeclaredMethods();
+    for (Method n : m) {
+      declaredMethods.add(n.getName());
+    }
+    declaredMethods.remove("main");
+    declaredMethods.remove("notFound");
+    declaredMethods.remove("invalidArgument");
+  }
 }
