d505d0872cc7d89fba23e95c9ef60b6caa85b172
joaoLouceiro <jrlouceiro@live.com>
2024-07-04 17:31:26 +0100

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -5,11 +6,11 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
   static List<String> declaredMethods = new ArrayList<>();
+  static String filePath = "";
 
   public static void main(String[] args) throws Exception {
 
@@ -21,6 +22,7 @@ public class Main {
       String input = scanner.nextLine();
       String[] shellArgs = input.split(" ");
       isRunning = callCommand(shellArgs);
+      filePath = "";
     } while (isRunning);
     scanner.close();
   }
@@ -34,21 +36,34 @@ public class Main {
       case "type":
         return type(args);
       default:
+        if (programExists(args[0])) {
+          return runExternalProgram(args);
+        }
         return notFound(args[0]);
     }
   }
 
-  private static boolean isInPath(String command) {
+  private static boolean programExists(String command) {
     String[] pathEnv = System.getenv("PATH").split(":");
     for (String p : pathEnv) {
-      String filePath = p + "/" + command;
-      Path path = Paths.get(filePath);
+      String tmpFileName = p + "/" + command;
+      Path path = Paths.get(tmpFileName);
       if (Files.exists(path)) {
-        System.out.printf("%s is %s\n", command, filePath);
+        filePath = tmpFileName;
         return true;
       }
     }
     return false;
+  }
+
+  private static boolean runExternalProgram(String[] args) {
+    try {
+      Runtime.getRuntime().exec(args);
+    } catch (IOException e) {
+      System.err.println("Something went wrong");
+      e.printStackTrace();
+    }
+    return true;
   }
 
   private static boolean type(String[] args) {
@@ -60,7 +75,8 @@ public class Main {
       return true;
     }
 
-    if (isInPath(args[1])) {
+    if (programExists(args[1])) {
+      System.out.printf("%s is %s\n", args[1], filePath);
       return true;
     }
 
